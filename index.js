e4e70f803af31359c998c3bfe65dc1d1fc9aaedb
Łukasz Kieś <kiesiu@kiesiu.com>
2016-11-17 21:22:50 +0100

    Fix TypeError when dirs is undefined

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var Task = Elixir.Task;
 Elixir.extend('cleanup', function(dirs) {
   var default_dirs = ['public/css', 'public/js', 'public/build'];
   var custom_dirs = dirs || [];
-  var all_dirs = default_dirs.concat(dirs);
+  var all_dirs = default_dirs.concat(custom_dirs);
 
   new Task('cleanup', function(cb) {
     del(all_dirs, cb);
